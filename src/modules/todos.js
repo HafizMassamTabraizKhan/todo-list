040baf40ba6ad2295974c993fb674b56f046fd08
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-05-04 17:32:46 +0500

    Complete list structure

---
+++ src/modules/todos.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: 1,
+    description: 'attend morning session at 12 pm',
+    compeleted: false,
+  },
+  {
+    id: 2,
+    description: 'finish todo project',
+    compeleted: false,
+  },
+  {
+    id: 3,
+    description: 'attend standup session at 9 pm',
+    compeleted: false,
+  },
+];
+
+export default todos;
