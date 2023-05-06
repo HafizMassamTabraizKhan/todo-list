e95b6a7e0707d725a86352ec5a27a6f07e4c4a78
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-05-06 12:41:18 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
+    clean: true
   },
   optimization: {
     runtimeChunk: 'single',
